53c45e14415c5c94e788db2a416807458c8b4cee
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-02-05 19:10:22 +0530

    Add student details input and marks calculation

---
+++ studnets details.py
@@ -0,0 +1,13 @@
+name=input("Enter the studnet name :")
+usn=input("Enter the USN :")
+print("Enter the marks obtiend each subject ")
+sub1=int(input("Enter the marks of subject 1 :"))
+sub2=int(input("Enter the marks of subject 2 :"))
+sub3=int(input("Enter the marks of subject 3 :"))
+sub4=int(input("Enter the marks of subject 4 :"))
+sum=sub1+sub2+sub3+sub4
+avg=sum/4
+print("Name :",name)
+print("USN :",usn)
+print("Total marks =",sum)
+print("Average marks =",avg)
