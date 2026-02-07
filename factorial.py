76c893ecdd4f1605a0aed266a430612db62e2078
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-02-07 22:42:22 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -4,7 +4,7 @@ print(random.randint(0,9))
 
 #check even or odd
 num=int(input("Enter a number :"))
-if(num%2)==0:
+if(num%2)==0: 
   print("{0} is even ",format(num))
 else:
   print("{0} is odd",format(num))
