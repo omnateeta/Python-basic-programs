64d6b3d9a95cf1da8680de3447c1be63a9a7cf89
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-02-07 21:40:56 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,15 @@
+#generate a random number
+import random
+print(random.randint(0,9))
+
+#check even or odd
+num=int(input("Enter a number :"))
+if(num%2)==0:
+  print("{0} is even ",format(num))
+else:
+  print("{0} is odd",format(num))
+
+#factorial in python
+import math
+n=3
+print(math.factorial(n))
