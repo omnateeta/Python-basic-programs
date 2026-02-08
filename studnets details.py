22bb36292e512abd2ef4ff537c16ccdee9f9c138
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-02-08 17:32:56 +0530

    Update studnets details.py

--- studnets details.py
+++ studnets details.py
@@ -4,7 +4,7 @@ print("Enter the marks obtiend each subject ")
 sub1=int(input("Enter the marks of subject 1 :"))
 sub2=int(input("Enter the marks of subject 2 :"))
 sub3=int(input("Enter the marks of subject 3 :"))
-sub4=int(input("Enter the marks of subject 4 :"))
+sub4=int(input("Enter the marks of subject 4 :")) 
 sum=sub1+sub2+sub3+sub4
 avg=sum/4
 print("Name :",name)
