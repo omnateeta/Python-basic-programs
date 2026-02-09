d279847bc2aaa51def2167b7e8e44481d96f1c2a
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-02-09 14:13:48 +0530

    Fix indentation and improve temperature check message

--- if-elsein python.py
+++ if-elsein python.py
@@ -1,7 +1,8 @@
 #Simple temperature cheking using if-else codition in python
 temp = input("Enter the temperature : ")
 temp = int(temp)
-if temp>35:
+if temp>35: 
   print("It is empty")
 else:
   print("It is hot")
+ 
