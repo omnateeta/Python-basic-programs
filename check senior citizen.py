e1a8c5f059f8f707efea43e4137ec67441bb363c
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-02-10 21:23:23 +0530

    Correct if-else structure for senior citizen check
    Fix indentation and formatting issues in the code.

--- check senior citizen.py
+++ check senior citizen.py
@@ -1,6 +1,6 @@
 #This simple program demostarte the if-else codition to check the person is citizen or not based on the age. if age >= 60 then it will print the if block or else it will go through else part
 age=int(input("Enter the age :"))
-if (age>=60):
+if (age>=60):  
   print("Person is senior citizen")
 else:
   print("Person is not senior citizen")
