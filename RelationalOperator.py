f4e50aba27329324a6de9ab89e21249068d03f62
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-02-13 18:52:42 +0530

    Create RelationalOperator.py

---
+++ RelationalOperator.py
@@ -0,0 +1,18 @@
+Relational operators help you compare two values or variables. They give you a result of either True or False based on the comparison.
+
+Basic Comparison
+Let's start with a simple example:
+
+height1 = 12
+height2 = 14
+print(height1 < height2) # Outputs True because 12 is less than 14
+
+#In this example, the `<` operator compares `height1` and `height2`. Since `height1` is less than `height2`, it returns `True`.
+Other Relational Operators
+Here are some more examples of relational operators:
+
+- a > b   (Checks if `a` is greater than `b`)
+- a == b  (Checks if `a` is equal to `b`)
+- a != b  (Checks if `a` is not equal to `b`)
+- a >= b  (Checks if `a` is greater than or equal to `b`)
+- a <= b  (Checks if `a` is less than or equal to `b`)
