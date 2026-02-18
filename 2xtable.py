c0d7cf86d6d92ec3ab7224699d35c98207355407
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-02-18 22:14:24 +0530

    Create 2xtable.py

---
+++ 2xtable.py
@@ -0,0 +1,17 @@
+Write a program to print odd numbers between 
+10
+10 to 
+20
+20 on separate lines:
+
+Think of where the loop should start from, declare a variable a and initialise it to that value.
+Use the while syntax to create a loop,
+Think of what condition would stop the loop when the iterator reaches its end.
+Think of how to adjust the value of the iterator within each iteration. Would it be incremented or decremented? By what value should it be incremented or decremented?
+
+# cook your dish here
+a = 11
+while a < 20:
+    print(a)
+    a = a+2
+    
